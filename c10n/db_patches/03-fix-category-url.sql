a1563a3e7e1edb99902e64939dd9382a6326394a
darkside <andrey.garbuz@gmail.com>
2011-08-30 21:37:50 +0300

    Update db patch to add '/catalog' prefix in URL

--- c10n/db_patches/03-fix-category-url.sql
+++ c10n/db_patches/03-fix-category-url.sql
@@ -1,1 +1,2 @@
 update `items` set url = concat('/catalog', url) where id = 1017;
+update `items` set url = concat('/catalog', url) where url like '/pismennye-stoliki-razvivajuwie-party-dlja-detej/%';
