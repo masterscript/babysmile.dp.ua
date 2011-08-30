75831e845150b9452e142d6f3f3cf47d3d36e61e
darkside <andrey.garbuz@gmail.com>
2011-08-30 23:33:17 +0300

    Articles output

---
+++ plugins/getLastArticles.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @param current_page $obj
+ */
+function getLastArticles($obj) {
+    
+    $count = $obj->issetParam('count') ? $obj->getParam('count') : 5;
+    
+	$items = db::getDB()->select('
+		SELECT description,create_date,i.id,name,url,title,filename AS img_src FROM items i
+		LEFT JOIN top_images ti ON ti.id = i.id
+		WHERE type = ? and protected<=?d ORDER BY create_date DESC {limit ?d}',
+		'article', user::getAccessLevel(),$count
+    );
+    
+    foreach ($items as $item) {
+		$objectItems[]=new page($item);
+	}
+		
+	return $objectItems;
+}
