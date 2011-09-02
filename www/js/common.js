4a8c9712f435c6161429c84b29aa30ce11c381dd
darkside <andrey.garbuz@gmail.com>
2011-09-03 02:39:08 +0300

    Implement site search

--- www/js/common.js
+++ www/js/common.js
@@ -5,4 +5,21 @@ $(function() {
 	$('input:submit').button();
 	$('input:button').button();
 	
+	$('#formSearch').dialog({
+		modal: true,
+		autoOpen: false,
+		resizable: false,
+		width: 350,
+		show: 'explode',
+		hide: 'explode',
+		buttons: [
+			{ text: 'Поиск', click: function() { $(this).submit(); } },
+			{ text: 'Отмена', click: function() { $(this).dialog("close"); } }
+		]
+	});
+	$('#header_icons a.search').click(function() {
+		$('#formSearch').dialog('open');
+		return false;
+	});
+	
 });
