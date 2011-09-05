c67dc121733dd5e893aa3be801167a7f573a926b
darkside <andrey.garbuz@gmail.com>
2011-09-05 21:42:48 +0300

    Add db patch to remove all ajax pages

---
+++ c10n/db_patches/08-remove-ajax-pages.sql
@@ -0,0 +1,3 @@
+INSERT INTO items set pid=1, url='/discounts', type='container',name='Акции',title='Акции',template='discounts';
+INSERT INTO items set pid=1, url='/actual', type='article',name='Актуально',title='Актуально';
+INSERT INTO items set pid=1, url='/latest', type='container',name='Новинки',title='Новинки',template='goods_latest';
