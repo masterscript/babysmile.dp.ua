26bc22977d04c276bcaef8a0298bc82cc15f52e1
masterscript <evglebed@gmail.com>
2011-09-25 20:56:45 +0300

    edit toggle.js

--- www/js/toggle.js
+++ www/js/toggle.js
@@ -1,29 +1,6 @@
-        $(document).ready(function() {
-   $('#effect').hide();
-   $('#effect').children().hide;
-});
 	$(function() {
-		// run the currently selected effect
-		function runEffect() {
-			// get effect type from
-			var selectedEffect = $( "#effectTypes" ).val();
-
-			// most effect types need no options passed by default
-			var options = {};
-			// some effects have required parameters
-			if ( selectedEffect === "scale" ) {
-				options = { percent: 0 };
-			} else if ( selectedEffect === "size" ) {
-				options = { to: { width: 200, height: 60 } };
-			}
-
-			// run the effect
-			$( "#effect" ).toggle( selectedEffect, options, 500 );
-		};
-
-		// set effect from select menu value
 		$( "#desctoggle" ).click(function() {
-			runEffect();
+			$( "#effect" ).toggle( );
 			return false;
 		});
 	});
