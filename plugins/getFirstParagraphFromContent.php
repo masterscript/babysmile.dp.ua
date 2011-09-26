d18870c715532515f3a8c39fbc283198d5e239b3
masterscript <evglebed@gmail.com>
2011-09-26 13:16:12 +0300

    added other plugin getFirstParagraphFromContent

---
+++ plugins/getFirstParagraphFromContent.php
@@ -0,0 +1,18 @@
+﻿<?php
+function getFirstParagraphFromContent($obj)
+{
+
+		include_once SYSTEM_PATH.'/plugins/getContent.php';
+
+		$content=getContent($obj);
+		$a_str=split ("</p>",$content,2);
+
+		if (sizeof($a_str) <> 2 ) {
+			$a_str[0]=$content;
+			$a_str[1]='';
+
+		}
+
+		return $a_str;
+}
+?>
