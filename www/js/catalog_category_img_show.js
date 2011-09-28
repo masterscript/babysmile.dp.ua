c366dc08612477bc91b09421067834de971ebace
masterscript <evglebed@gmail.com>
2011-09-28 15:22:50 +0300

    Pictures on the front page

---
+++ www/js/catalog_category_img_show.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(document).ready(function(){
+        $('a.catalog_category').hover(
+            function() {
+                id = $(this).attr('id');
+                num = id.replace(/img_(\d+)/gi, '$1');
+                $('#src_img').attr('src', '/images/img_cat_eng/'+num+'.jpg');
+            },
+            function() { 
+            //    $('#src_img').attr('src', '/images/img_cat_eng/'+num+'.jpg');
+		 $('#src_img').attr('src', '/images/main_categories_image.jpg'); 
+            }
+        );    
+});
+
